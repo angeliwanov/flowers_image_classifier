4682d5199379758f74fbae6f7ad6a38a007d9205
Angel Ivanov <angel.iwanov@gmail.com>
2021-08-02 19:16:42 +0300

    Final commit

--- predict.py
+++ predict.py
@@ -3,7 +3,11 @@ import json
 import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
 from PIL import Image
+
+use_cuda = torch.cuda.is_available()
 
 def get_predict_args():
     parser = argparse.ArgumentParser()
@@ -18,7 +22,85 @@ def get_predict_args():
   
 predict_args = get_predict_args()
 
-model = torch.load(predict_args.checkpoint + '/model.pt')
+#Initializing model function
+def initialize_model(model_arch, num_of_categories, num_of_hidden_units=None):
+    model_ft = None
+    
+    arch_to_hidden_units = {'resnet50': 512, 'vgg16':512, 'alexnet':512}
+    
+    if model_arch == 'resnet50':
+        model_ft = models.resnet50(pretrained=True)
+    elif model_arch == 'vgg16':
+        model_ft = models.vgg16(pretrained=True)
+    elif model_arch == 'alexnet':
+        model_ft = models.alexnet(pretrained=True)
+    
+    if model_ft is not None:
+        
+        if num_of_hidden_units is None:
+            num_of_hidden_units = arch_to_hidden_units[model_arch]
+            
+        for param in model_ft.parameters():
+            param.requires_grad = False
+        
+        model_last_child = list(model_ft.children())[-1]
+        
+        if (isinstance(model_last_child, nn.modules.linear.Linear)):
+            in_features = model_last_child.in_features
+        else:
+            list_of_children = list(model_last_child.children())
+            for i in range(len(list_of_children)):
+                if (isinstance(list_of_children[i], nn.modules.linear.Linear)):
+                    in_features = model_last_child[i].in_features
+                    break
+        
+        my_classifier = nn.Sequential(nn.Linear(in_features, num_of_hidden_units),
+                                     nn.BatchNorm1d(num_of_hidden_units),
+                                     nn.ReLU(),
+                                     nn.Dropout(p=0.5),
+                                     nn.Linear(num_of_hidden_units, num_of_categories))
+        
+        model_ft.fc = model_ft.classifier = my_classifier
+        
+    return model_ft
+
+def load_checkpoint(filepath, use_cuda, gpu):
+    model = None
+    criterion = None
+    last_epoch = None
+    
+    try:
+        checkpoint = torch.load(filepath + '/model.pt', map_location=lambda storage, loc:storage)
+
+        model_arch = checkpoint['arch']
+        learning_rate = checkpoint['learning_rate']
+        num_of_hidden_units = checkpoint['num_of_hidden_units']
+        num_of_categories = checkpoint['num_of_categories']
+
+        model = initialize_model(model_arch, num_of_categories, num_of_hidden_units)
+
+        if model is not None:
+            if use_cuda and gpu:
+                device = torch.device('cuda')
+            else:
+                device = torch.device('cpu')
+
+            model.to(device)
+
+            optimizer = optim.Adam(model.fc.parameters(), learning_rate)
+            criterion = checkpoint['loss']
+            model.load_state_dict(checkpoint['state_dict'])
+            model.class_to_idx = checkpoint['class_to_idx']
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            last_epoch = checkpoint['epoch']
+    except:
+        print('Failed loading checkpoint')
+    
+    return model, criterion, last_epoch
+
+
+model, criterion, last_epoch = load_checkpoint(predict_args.checkpoint, use_cuda, predict_args.gpu)
+
 
 with open(predict_args.category_names, 'r') as f:
     cat_to_name = json.load(f)
@@ -35,21 +117,33 @@ def process_image(image):
                                  std=[0.229, 0.224, 0.225])
 
     
-    transform =  transforms.Compose([transforms.ToTensor(),normalize,])
+    transform =  transforms.Compose([transforms.ToTensor(),normalize])
     normalized_image = transform(cropped_image)
 
     return normalized_image
 
-def predict(image_path, model, n_topk):
+def predict(image_path, model, use_cuda, gpu, n_topk=5):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''
     model.eval()
-    output = model(process_image(image_path).unsqueeze_(0).cuda())
-    probs, classes = output.topk(n_topk)
+    
+    if use_cuda and gpu:
+        print('Using GPU')
+        model = model.cuda()
+        image = process_image(image_path).unsqueeze_(0).cuda()
+        output = model(image)
+    else:
+        print('Using CPU')
+        output = model(process_image(image_path).unsqueeze_(0))
+    
+    output = torch.exp(output)
+    total = torch.sum(output)
+    result = torch.div(output, total)
+    probs, classes = result.topk(n_topk)
     
     return probs.tolist()[0], classes.tolist()[0]
 
-probs, classes = predict(predict_args.input, model, predict_args.top_k)
+probs, classes = predict(predict_args.input, model, use_cuda, predict_args.gpu, predict_args.top_k)
 names = [cat_to_name[k] for k, v in model.class_to_idx.items() if v in classes]
 
 
--- train.py
+++ train.py
@@ -17,10 +17,10 @@ def get_train_args():
     
     parser.add_argument('data_dir', type=str, help='Data Directory')
     parser.add_argument('--save_dir', type=str, default='checkpoint', help='Saving Directory')
-    parser.add_argument('--arch', type=str, default='vgg16', help='CNN architecture - vgg16 or alexnet')
+    parser.add_argument('--arch', type=str, default='vgg16', help='CNN architecture - vgg16 or alexnet or resnet50')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
-    parser.add_argument('--hidden_unit', type=int, default=1024, help='Number of hidden units')
-    parser.add_argument('--epochs', type=int, default=1, help='Number of epochs')
+    parser.add_argument('--hidden_unit', type=int, default=512, help='Number of hidden units')
+    parser.add_argument('--epochs', type=int, default=3, help='Number of epochs')
     parser.add_argument('--gpu', action='store_true', help='Use GPU')
     
     return parser.parse_args()
@@ -60,11 +60,16 @@ use_cuda = torch.cuda.is_available()
 def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, gpu, save_path):
     
     valid_loss_min = np.Inf
+    train_acc_log = []
+    valid_acc_log = []
+    train_loss_log = []
+    valid_loss_log = []
     
     for epoch in range(1, n_epochs+1):
         
         train_loss = 0
-        valid_loss = 0
+        train_corr = 0
+        train_total = 0
         
         model.train()
         
@@ -80,10 +85,16 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, gpu, save_pa
             optimizer.step()
             
             train_loss = train_loss + ((1/(batch_idx+1)) * (loss.data - train_loss))
+            
+            pred = output.data.max(1, keepdim=True)[1]
+            train_corr += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+            train_total += data.size(0)
+        
+        valid_loss = 0
+        valid_corr = 0
+        valid_total = 0
         
         model.eval()
-        correct = 0
-        total = 0
         
         for batch_idx, (data, target) in enumerate(loaders['valid']):
             
@@ -94,56 +105,100 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, gpu, save_pa
             loss = criterion(output, target)
             
             valid_loss = valid_loss + ((1/(batch_idx+1)) * (loss.data - valid_loss))
+            
+            pred = output.data.max(1, keepdim=True)[1]
+            valid_corr += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+            valid_total += data.size(0)
         
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
-        total += data.size(0)
+        print(f"Epoch: {epoch} Training loss: {train_loss:.6f} Validation loss {valid_loss:.6f}")
+        print(f"Training Accuracy: {100*train_corr/train_total:.6f} % ({train_corr} / {train_total}) Validation Accuracy: {100*valid_corr/valid_total:.6f} % ({valid_corr} / {valid_total})")
         
-        print(f"Epoch: {epoch} Training loss: {train_loss:.6f} Validation loss {valid_loss:.6f} Accuracy: {100*correct/total:.6f} % ({correct} / {total})")
+        train_loss_log.append(train_loss)
+        valid_loss_log.append(valid_loss)
+        train_acc_log.append(100*train_corr/train_total)
+        valid_acc_log.append(100*valid_corr/valid_total)
+        
         
         if valid_loss < valid_loss_min:
-            model.class_to_idx = image_datasets['train'].class_to_idx
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
-            torch.save(model, save_path + '/model.pt')
-            
+            torch.save(model.state_dict(), save_path+'/model.pt')
             valid_loss_min = valid_loss
+        
         
     return model
 
-#
-if train_args.arch == 'alexnet':
-    model = models.alexnet(pretrained=True)
-    in_features = model.classifier[1].in_features
-else:
-    model = models.vgg16(pretrained=True)
-    in_features = model.classifier[0].in_features
+#Initializing model function
+def initialize_model(model_arch, num_of_categories, num_of_hidden_units=None):
+    model_ft = None
+    
+    arch_to_hidden_units = {'resnet50': 512, 'vgg16':512, 'alexnet':512}
+    
+    if model_arch == 'resnet50':
+        model_ft = models.resnet50(pretrained=True)
+    elif model_arch == 'vgg16':
+        model_ft = models.vgg16(pretrained=True)
+    elif model_arch == 'alexnet':
+        model_ft = models.alexnet(pretrained=True)
+    
+    if model_ft is not None:
+        
+        if num_of_hidden_units is None:
+            num_of_hidden_units = arch_to_hidden_units[model_arch]
+            
+        for param in model_ft.parameters():
+            param.requires_grad = False
+        
+        model_last_child = list(model_ft.children())[-1]
+        
+        if (isinstance(model_last_child, nn.modules.linear.Linear)):
+            in_features = model_last_child.in_features
+        else:
+            list_of_children = list(model_last_child.children())
+            for i in range(len(list_of_children)):
+                if (isinstance(list_of_children[i], nn.modules.linear.Linear)):
+                    in_features = model_last_child[i].in_features
+                    break
+        
+        my_classifier = nn.Sequential(nn.Linear(in_features, num_of_hidden_units),
+                                     nn.BatchNorm1d(num_of_hidden_units),
+                                     nn.ReLU(),
+                                     nn.Dropout(p=0.5),
+                                     nn.Linear(num_of_hidden_units, num_of_categories))
+        
+        model_ft.fc = model_ft.classifier = my_classifier
+        
+    return model_ft
 
-for param in model.parameters():
-    param.requires_grad = False
+#Initialize model
+model = initialize_model(train_args.arch, 102, train_args.hidden_unit)
 
-n = train_args.hidden_unit
-
-classifier = nn.Sequential(OrderedDict([
-    ('fc1', nn.Linear(in_features, n)),
-    ('batch1', nn.BatchNorm1d(n)),
-    ('relu1', nn.ReLU()),
-    ('dropout1', nn.Dropout(p=0.5)),
-    ('fc2', nn.Linear(n, int(n/2))),
-    ('batch2', nn.BatchNorm1d(int(n/2))),
-    ('relu2', nn.ReLU()),
-    ('dropout2', nn.Dropout(p=0.5)),
-    ('output', nn.Linear(int(n/2), 102))
-]))
-
-model.classifier = classifier
-
-if use_cuda and train_args.gpu:
+if use_cuda:
     model = model.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.classifier.parameters(), lr=train_args.learning_rate)
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+
+model = train(train_args.epochs, loaders, model, optimizer, criterion, use_cuda, train_args.gpu, 'min_valid_loss') 
 
 
-model = train(train_args.epochs, loaders, model, optimizer, criterion, use_cuda, train_args.gpu, train_args.save_dir)
-
+def save_checkpoint(model, optimizer, criterion, arch, learning_rate, num_of_hidden_units, 
+                   num_of_categories, class_to_idx, save_path, last_epoch):
+    
+    checkpoint = {'epoch': last_epoch,
+                  'class_to_idx': class_to_idx,
+                  'state_dict': model.state_dict(),
+                  'optimizer': optimizer.state_dict(),
+                  'loss': criterion,
+                  'arch': arch,
+                  'learning_rate': learning_rate,
+                  'num_of_hidden_units': num_of_hidden_units,
+                  'num_of_categories': num_of_categories
+    }
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    
+    torch.save(checkpoint, save_path+'/model.pt')
+  
+save_checkpoint(model, optimizer, criterion, train_args.arch, train_args.learning_rate, train_args.hidden_unit, 102, image_datasets['train'].class_to_idx, train_args.save_dir, train_args.epochs)
